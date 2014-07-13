aef49e7e4e091ea5b6bdb960e74625c5048b0233
Chris Ollenburg <chris.ollenburg@gmail.com>
2014-07-13 00:29:39 -0400

    Implemented most of the coupon detail view and layout.

--- app/src/main/java/tme/transactthis/com/couponquest/CouponDetailActivity.java
+++ app/src/main/java/tme/transactthis/com/couponquest/CouponDetailActivity.java
@@ -3,7 +3,11 @@ package tme.transactthis.com.couponquest;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 import tme.transactthis.com.couponquest.model.ICoupon;
 
 /**
@@ -12,14 +16,34 @@ import tme.transactthis.com.couponquest.model.ICoupon;
 public class CouponDetailActivity extends Activity {
     private static final String TAG = CouponDetailActivity.class.getSimpleName();
 
+    @InjectView(R.id.coupon_title_tv)
+    TextView couponTitle;
+    @InjectView(R.id.coupon_dollar_amount_tv)
+    TextView couponDollarAmount;
+    @InjectView(R.id.coupon_detail_tv)
+    TextView couponDetail;
+    @InjectView(R.id.coupon_disclaimer_tv)
+    TextView couponDisclaimer;
+
+
     private ICoupon mCoupon;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.coupon_detail_activity);
+        ButterKnife.inject(this);
 
-//        Intent intent = getIntent();
-//        mCoupon  = (ICoupon) intent.getSerializableExtra(getString(R.string.COUPON_KEY));
+        Intent intent = getIntent();
+        mCoupon  = (ICoupon) intent.getSerializableExtra(getString(R.string.COUPON_KEY));
+
+        couponTitle.setText(mCoupon.getShortDescription());
+        couponDollarAmount.setText(mCoupon.getCategoryValue());
+        couponDetail.setText(mCoupon.getDescription());
+        couponDisclaimer.setText(mCoupon.getTerms());
+    }
+
+    @OnClick(R.id.unlock_button) void submit() {
+        //TODO Unlock this coupon
     }
 
 }
